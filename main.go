b7aecec88e5ac714b0c1cbe08c73f7a7e5f60ba2
Kurniawan Aryanto Famili <kurnifamili@gmail.com>
2020-01-09 22:15:47 +0800

    test push

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", TestServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+func TestServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "TEST! Subpath %s", r.URL.Path[1:])
+}
